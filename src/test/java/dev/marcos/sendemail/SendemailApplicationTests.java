a8a3dc3883c684bde01bd371ec92e72a82a3b0ef
Marcos Allan <marcosallanm1to@gmail.com>
2025-12-29 09:17:58 -0300

    chore: init project with Spring Boot, Java Mail, RabbitMQ and Lombok

---
+++ src/test/java/dev/marcos/sendemail/SendemailApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcos.sendemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
